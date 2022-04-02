8146a45931964f20889decc511b0133c08514829
Alex Nekrasov <alexandrnekrasov1988@gmail.com>
2022-04-02 16:55:28 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+        int balance = 0; // текущий баланс
+        int money = 1500 ; // сумма пополнения
+        int bonus = 100; // бонус за каждые 100р равен 1р
+        System.out.println("Баланс абонента равен:");
+        if (money > 1000) {
+            System.out.println(balance + money + (money / bonus));
+            System.out.println("Получено бонусов:");
+            System.out.println(money/bonus);}
+       else {
+            System.out.println(balance + money);
+
+        }
+
+    }
+
+}
+
+
